ceb5af3125e74a4369dca71cba4feeef950286bd
Cat Luong <luongcat162002@gmail.com>
2024-04-16 17:13:33 -0400

    Refractor repository

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ from typing import Union
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from flask_cors import CORS
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app)
@@ -23,7 +25,7 @@ def get_db_connection():
 def getHousehold():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM dbo.Households")
+    cursor.execute("SELECT * FROM [dbo].['Households']")
     results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
     conn.close()
     return jsonify(results)
@@ -32,7 +34,37 @@ def getHousehold():
 def getHouseholdByID(household_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM dbo.Households WHERE Hshd_num = ?", (household_id,))
+    cursor.execute("SELECT * FROM [dbo].['Households'] WHERE Hshd_num = ?", (household_id,))
+    result = cursor.fetchone()
+    if result is None:
+        abort(404, description="Household not found")
+    result = dict(zip([column[0] for column in cursor.description], result))
+    conn.close()
+    return jsonify(result)
+
+@app.route('/getAll', methods=['GET'])
+def getAllTables():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT *
+                    FROM [dbo].[Households] AS hh
+                    JOIN [dbo].[Transactions20k] AS tr ON hh.Hshd_num = tr.Hshd_num
+                    JOIN [dbo].[Products] AS pr ON tr.Product_num = pr.Product_num;
+                   """)
+    results = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
+    conn.close()
+    print(len(results))
+    return jsonify(results)
+
+@app.route('/getAllHouseholdbyID/<int:household_id>', methods=['GET'])
+def getAllHouseholdbyID(household_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""SELECT *
+                    FROM [dbo].[Households] AS hh
+                    JOIN [dbo].[Transactions20k] AS tr ON hh.Hshd_num = tr.Hshd_num
+                    JOIN [dbo].[Products] AS pr ON tr.Product_num = pr.Product_num
+                    WHERE hh.Hshd_num = ?""", (household_id,))
     result = cursor.fetchone()
     if result is None:
         abort(404, description="Household not found")
